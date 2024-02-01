2226021127620685a015d305db4420455bcaff3c
Abhranil-01 <abhranilkundu06@gmail.com>
2024-02-01 14:29:36 +0530

    commit

---
+++ Frontend/src/Pages/TestBooking/TestBooking.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestBooking() {
+  return (
+    <div>
+      <h1>Test Booking</h1>
+    </div>
+  )
+}
---
+++ Frontend/src/Pages/index.js
@@ -0,0 +1,5 @@
+
+export {default as Home } from './Home/Home.jsx';
+export {default as TestBooking} from './TestBooking/TestBooking.jsx';
+export {default as MedicineStore } from './MedicineStore/MedicineStore.Jsx';
+export {default as About} from './About/About.jsx';
